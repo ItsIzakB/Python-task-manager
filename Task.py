be3a2e293b01e35c82e8b4cea151472c6f8cdfb7
izak <kazipriom@gmail.com>
2024-12-02 16:31:06 -0600

    task class

--- Task.py
+++ Task.py
@@ -1,5 +1,18 @@
+import self as self
+
 import Project
 class Task(Project):
 
-    def _init_(self):
-        pass
+    def __init__(self, proj_name, date, task_name):
+        super().__init__(proj_name, date)
+        self.name = task_name
+
+    def getInfo(self):
+        print(f'ProjName: {self.proj_name}, '
+              f'Proj_date: {self.date}, '
+              f'task_name: {self.task_name}')
+
+
+myT = Task('Proj A', '11', 'T1')
+
+myT.get
