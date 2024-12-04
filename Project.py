d40e7596be1177556ee0e32bdd6bf8cf1a3cd0bc
izak <kazipriom@gmail.com>
2024-12-04 13:56:12 -0600

    finished iterator

--- Project.py
+++ Project.py
@@ -8,9 +8,17 @@ class ProjectClass:
     def getProjName(self) -> str:
         return self.name
     def __iter__(self):
-        return self.tasks
+        self.index = 0
+        return self
 
     def __next__(self):
+        if (self.index>= len(self.tasks)):
+            return StopIteration
+        else:
+            task = self.tasks[self.index]
+            self.index+=1
+            return task
+
 
 
 
