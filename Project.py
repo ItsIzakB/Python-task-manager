dc1ebbb901ff2e8b939e6fd48fc55876c207f0f5
izak <kazipriom@gmail.com>
2024-12-04 14:00:34 -0600

    main testing

--- Project.py
+++ Project.py
@@ -22,7 +22,6 @@ class ProjectClass:
 
 
 
-
 print("this is project class")
 
 
--- main.py
+++ main.py
@@ -6,7 +6,15 @@ def main():
 
     task1 = tsk.Task('11/23/2025', ' 1500 Applications')
 
-    print(isinstance(task1.get_info()))
+    task2 = tsk.Task('04/12/2025', 'LowCalProj Finish')
+
+    myP.addTask(task1)
+
+    myP.addTask(task2)
+
+    for task in myP:
+        task.getInfo()
+
 
 
 if __name__ == 'main': #TODO Need this to work
