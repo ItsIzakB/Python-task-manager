e1c393e04b0ac3edfa2fa1d79b362bfee502c6f6
izak <kazipriom@gmail.com>
2024-12-03 21:49:36 -0600

    main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import Project as pj
-import Task
+import Task as tsk
 def main():
 
-    myP = pj.ProjectClass("SWE Workshop")
+    myP = pj.ProjectClass("SWE Intership Grind")
 
-    task1 = Task.Task()
+    task1 = tsk.Task('11/23/2025', ' 1500 Applications')
+
+
+    task1.getInfo()
 
 
