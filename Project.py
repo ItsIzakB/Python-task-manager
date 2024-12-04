75630fb6d5af639266badb08f17e6f6f86591c21
izak <kazipriom@gmail.com>
2024-12-03 18:10:12 -0600

    created func

--- Project.py
+++ Project.py
@@ -1,9 +1,12 @@
+import Task
 class ProjectClass:
 
     def __init__(self, name, date):
         self.name = name
         self.date = date
-
+        self.tasks = []
+    def addTask(self, task):
+        self.tasks.append(task)
     def getProjName(self) -> str:
         return self.name
 
